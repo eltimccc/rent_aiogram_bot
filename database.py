a8e1a199e36e0b0a53a9e628b0ee559684404fc9
Денис <eltimccc@yandex.ru>
2023-03-09 15:15:24 +0300

    время часы минуты

--- database.py
+++ database.py
@@ -1,15 +1,18 @@
 from sqlalchemy import Column, Integer, String, create_engine, DateTime, text
 from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
 
 
 engine = create_engine('sqlite:///my_database.db')
 Base = declarative_base()
+current_time = datetime.now()
+
 
 class CallOrder(Base):
     __tablename__ = 'call_orders'
 
     id = Column(Integer, primary_key=True)
-    date = Column(DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    date = Column(DateTime, server_default=current_time.strftime('%Y-%m-%d %H:%M'))
     user_name = Column(String)
     phone_number = Column(String)
 
